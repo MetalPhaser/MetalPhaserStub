8ebe5a2ea51c6a859c24168a9ed2e2b0445c14e5
mmaher <matt.maher@am.sony.com>
2015-09-13 17:36:43 -0700

    - updated with MetalKeyboard and game-config

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,61 +1,68 @@
-let game;
-let player;
-let platforms;
-let cursors;
-let jumpButton;
+import Config from '../config/game-config';
+import Keyboard from '../utils/Keyboard';
 
-let gravityStrength     = 1200;
-let jumpStrength        = 650;
-let moveStrength        = 250;
-let playerBounciness    = 0.2;
 
 class GameState extends Phaser.State {
-	preload() {
-        game                            = this.game;
-        game.stage.backgroundColor      = '#85b5e1';
-        game.load.baseURL               = 'http://examples.phaser.io/assets/';
-        game.load.crossOrigin           = 'anonymous';
 
-		game.load.image('player', 'sprites/phaser-dude.png');
-		game.load.image('platform', 'sprites/platform.png');
+	constructor() {
+		super();
+
+		this.player                 = null;
+		this.platforms              = null;
+		this.cursors                = null;
+		this.jumpButton             = null;
+
+		this.gravityStrength        = 1200;
+		this.jumpStrength           = 650;
+		this.moveStrength           = 250;
+		this.playerBounciness       = 0.2;
+	}
+
+	preload() {
+    this.game.stage.backgroundColor      = '#85b5e1';
+    this.game.load.baseURL               = 'http://examples.phaser.io/assets/';
+    this.game.load.crossOrigin           = 'anonymous';
+		this.game.load.image('player', 'sprites/phaser-dude.png');
+		this.game.load.image('platform', 'sprites/platform.png');
 	}
 
 	create() {
 
-        cursors         = game.input.keyboard.createCursorKeys();
-        jumpButton      = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.cursors         = this.game.input.keyboard.createCursorKeys();
+		this.jumpButton      = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 		//  Set the world (global) gravity
 		//game.physics.arcade.gravity.y = 1000;
 
-        platforms       = game.add.physicsGroup();
-		platforms.create(500, 150, 'platform');
-		platforms.create(-200, 300, 'platform');
-		platforms.create(400, 450, 'platform');
-		platforms.setAll('body.immovable', true);
+		this.platforms       = this.game.add.physicsGroup();
+		this.platforms.create(500, 150, 'platform');
+		this.platforms.create(-200, 300, 'platform');
+		this.platforms.create(400, 450, 'platform');
+		this.platforms.setAll('body.immovable', true);
 
-		player 			= game.add.sprite(100, 0, 'player');
-		game.physics.arcade.enable(player);
-		player.body.collideWorldBounds = true;
-		player.body.bounce.y = playerBounciness;
-		player.body.gravity.y = gravityStrength;
+		this.player         = this.game.add.sprite(100, 0, 'player');
+
+		this.game.physics.arcade.enable(this.player);
+		this.player.body.collideWorldBounds = true;
+		this.player.body.bounce.y     = this.playerBounciness;
+		this.player.body.gravity.y    = this.gravityStrength;
 
 	}
 
 	update() {
-		game.physics.arcade.collide(player, platforms);
+		this.game.physics.arcade.collide(this.player, this.platforms);
 
-		player.body.velocity.x = 0;
+		this.player.body.velocity.x = 0;
 
-		if (cursors.left.isDown) {
-			player.body.velocity.x = -(moveStrength);
+		if (this.cursors.left.isDown) {
+			this.player.body.velocity.x = -(this.moveStrength);
 		}
-		else if (cursors.right.isDown) {
-			player.body.velocity.x = moveStrength;
+		else if (this.cursors.right.isDown) {
+			this.player.body.velocity.x = this.moveStrength;
 		}
 
-		if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
-			player.body.velocity.y = -(jumpStrength);
+		if (this.jumpButton.isDown && (this.player.body.onFloor() || this.player.body.touching.down)) {
+			this.player.body.velocity.y = -(this.jumpStrength);
 		}
 		//else if (jumpButton.isDown) {
 		//	player.body.velocity.y = -150;
