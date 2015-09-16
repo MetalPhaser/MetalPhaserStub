9a421d3125b22223c5abe8b70ce3fb007d340504
mmaher <matt.maher@am.sony.com>
2015-09-16 06:23:33 -0700

    - Cleaner Keyboard utils - Stub State - Keyboard handling more explicit

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,17 +1,21 @@
 import Config from '../config/game-config';
-import Keyboard from '../utils/Keyboard';
-
 
 class GameState extends Phaser.State {
 
 	constructor() {
 		super();
 
+		// keys
+		this.leftKey                = null;
+		this.rightKey               = null;
+		this.jumpKey                = null;
+		this.escKey                 = null;
+
+		// sprites
 		this.player                 = null;
 		this.platforms              = null;
-		this.cursors                = null;
-		this.jumpButton             = null;
 
+		// configuration
 		this.gravityStrength        = 1200;
 		this.jumpStrength           = 650;
 		this.moveStrength           = 250;
@@ -19,17 +23,21 @@ class GameState extends Phaser.State {
 	}
 
 	preload() {
-    this.game.stage.backgroundColor      = '#85b5e1';
-    this.game.load.baseURL               = 'http://examples.phaser.io/assets/';
-    this.game.load.crossOrigin           = 'anonymous';
+		this.game.stage.backgroundColor      = '#85b5e1';
+		this.game.load.baseURL               = 'http://examples.phaser.io/assets/';
+		this.game.load.crossOrigin           = 'anonymous';
 		this.game.load.image('player', 'sprites/phaser-dude.png');
 		this.game.load.image('platform', 'sprites/platform.png');
 	}
 
 	create() {
 
-		this.cursors         = this.game.input.keyboard.createCursorKeys();
-		this.jumpButton      = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.leftKey          = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		this.rightKey         = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+		this.jumpKey          = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.escKey           = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+
+		this.escKey.onDown.add(this.onEscapePress);
 
 		//  Set the world (global) gravity
 		//game.physics.arcade.gravity.y = 1000;
@@ -49,24 +57,30 @@ class GameState extends Phaser.State {
 
 	}
 
+	shutdown() {}
+
 	update() {
 		this.game.physics.arcade.collide(this.player, this.platforms);
 
 		this.player.body.velocity.x = 0;
-
-		if (this.cursors.left.isDown) {
+		//console.log('this.leftKey', this.leftKey.isDown, this.leftKey);
+		if (this.leftKey.isDown) {
 			this.player.body.velocity.x = -(this.moveStrength);
 		}
-		else if (this.cursors.right.isDown) {
+		else if (this.rightKey.isDown) {
 			this.player.body.velocity.x = this.moveStrength;
 		}
 
-		if (this.jumpButton.isDown && (this.player.body.onFloor() || this.player.body.touching.down)) {
+		if (this.jumpKey.isDown && (this.player.body.onFloor() || this.player.body.touching.down)) {
 			this.player.body.velocity.y = -(this.jumpStrength);
 		}
 		//else if (jumpButton.isDown) {
 		//	player.body.velocity.y = -150;
 		//}
+	}
+
+	onEscapePress() {
+		console.log('you pressed ESC');
 	}
 
 }
